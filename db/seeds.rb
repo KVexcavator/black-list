aa3c1c1ce90f1b7f6e22758a591349f9a2f4df06
excavator <str_token@mail.ru>
2019-09-03 16:17:59 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Costomer.create(name: 'Sasha',phone: '3434343', discription: 'the record creation needed to seed the database with its default values.
+# The data can then be loaded with the', blacklist: true)
+Costomer.create(name: 'Vacul',phone: '4767657', discription: 'the record creation needed to seed the database with its default values.
+# The data can then be loaded with the', blacklist: true)
+Costomer.create(name: 'Sracul',phone: '8787868', discription: 'the record creation needed to seed the database with its default values.
+# The data can then be loaded with the', blacklist: false)
+Costomer.create(name: 'Tobic',phone: '868686', discription: 'the record creation needed to seed the database with its default values.
+# The data can then be loaded with the', blacklist: true)
+Costomer.create(name: 'Bobik',phone: '8686786', discription: 'the record creation needed to seed the database with its default values.
+# The data can then be loaded with the', blacklist: false)
